239519b36621594d69452df430fdd986e2c6e256
dcts <thomas@liist.com>
2021-07-19 01:23:14 +0200

    :wrench: basic setup donw

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import Tenor from './services/Tenor.js';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: true,
+      counter: 0,
+    }
+
+    this.tenor = new Tenor("NBHUCIKD8UJ7");
+    this.tenor.search("Puppy").then(res => {
+      console.log(res);
+    })
+  }
+
+  handleClick = () => {
+    console.log("clicked");
+    this.setState({
+      clicked: !this.state.clicked,
+      counter: this.state.counter+1,
+    });
+  }
+  render() {
+    return (
+      <div className={this.state.clicked ? "clicked" : null}
+        onClick={this.handleClick}>
+        Hello,
+        {this.props.name} ({this.state.counter})
+      </div>
+    )
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
